20220420e889cf746480a46ddd0c700993dedde3
MegasesinoxJr <5582574@alu.murciaeduca.es>
2025-06-08 02:39:50 +0200

    a39

--- REACT/src/Paginas/FormularioInsignias.jsx
+++ REACT/src/Paginas/FormularioInsignias.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import axiosInstanceInsignias from "../axiosInstanceFormularios";
 
@@ -18,7 +18,7 @@ export default function CrearFormulario() {
   const [imagenPreview, setImagenPreview] = useState(null); // Vista previa de la imagen
 
   // Mantener respuestasNecesarias <= preguntas.length
-  React.useEffect(() => {
+  useEffect(() => {
     if (respuestasNecesarias > preguntas.length) {
       setRespuestasNecesarias(preguntas.length);
     }
